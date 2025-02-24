bf9b610d2a334e4b42ab5b19698688e998ee8d8d
bence <bence.arvai@outlook.com>
2025-02-24 02:02:25 +0100

    add test runner

---
+++ src/test/java/runners/RunCucumberTest.java
@@ -0,0 +1,11 @@
+package runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "stepdefs"
+)
+public class RunCucumberTest extends AbstractTestNGCucumberTests {
+}
